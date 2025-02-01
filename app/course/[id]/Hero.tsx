d57df83c17eadea789a02873b4462fab506005ce
unknown <minhajsulmuneeb@gmail.com>
2025-02-01 18:06:38 +0500

    updateing course page hero background

--- app/course/[id]/Hero.tsx
+++ app/course/[id]/Hero.tsx
@@ -12,9 +12,9 @@ type course = {
 const Hero = ({course} : {course :course}) => {
     return(
         <div style={{backgroundImage: `url(${course.image})`}}
-        className="min-h-[clac(65dvh+70px)] w-full z-10 text-primary flex bg-cover justify-center relative bg-center ">
+        className="min-h-[clac(65vh+70px)] w-full z-10 text-primary flex bg-cover justify-center relative bg-center ">
             <div className="bg-white dark:bg-black absolute top-0 right-0 left-0 bottom-0 opacity-65 dark:opacity-65 z-20  backdrop-blur-md "></div>
-            <div className="w-11/12 my-[20dvh] mt-[calc(20dvh+70px)] z-30 text-primary ">
+            <div className="w-11/12 my-[20dvh] mt-[calc(20vh+70px)] z-30 text-primary ">
                 <h1 className="text-4xl font-bold  ">
                     {course.name}
                 </h1>
