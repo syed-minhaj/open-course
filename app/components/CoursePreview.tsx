5c1d01c57bcfc1ace36fa179928df7395a4c3c3f
unknown <minhajsulmuneeb@gmail.com>
2025-02-21 00:37:24 +0500

    fixing margin of module count

--- app/components/CoursePreview.tsx
+++ app/components/CoursePreview.tsx
@@ -28,13 +28,13 @@ const CoursePreviw = ({course, index, admin } : {course: Course, index: number, 
                 <p className="h-32 md:h-auto max-h-32 min-h-24 overflow-y-clip font-light text-sm opacity-65 ">
                     {course.description}
                 </p>
-                <div className="md:hidden p-1 text-accent border border-accent rounded-md flex flex-row 
-                gap-1 h-[22] relative text-sm/4 w-fit ">
+                <div className="md:hidden p-1 text-accent border border-accent rounded-md flex flex-row items-center 
+                gap-1 relative text-sm/4 w-fit ">
                     <Package size={14} className="w-[14] h-[14]  "/> {course.modules.length}
                 </div>
             </div>
             <div className="md:flex flex-row justify-between hidden gap-3 text-sm ">
-                <div className="p-1 text-accent border border-accent rounded-md flex flex-row gap-1 h-[22] relative text-sm/4 ">
+                <div className="p-1 text-accent border border-accent rounded-md flex flex-row items-center gap-1 relative text-sm/4 ">
                     <Package size={14} className="w-[14] h-[14]  "/> {course.modules.length}
                 </div>
                 <Link href={`/course/${course.id}`} className="md:ml-auto text-accent hover:opacity-65 h-fit my-auto ">
