99793b03c2970e5256b199f315a91b52cd7e8ef3
unknown <minhajsulmuneeb@gmail.com>
2025-04-19 11:29:24 +0500

    fixing dark mode flashing

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -2,27 +2,13 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { useState, useEffect, type ReactNode } from "react";
+import { type ReactNode } from "react";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-  const [mounted, setMounted] = useState(false);
-
-  // Preventing hydration error
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <SessionProvider>
-        {children}
-      </SessionProvider>
-    );
-  }
 
   return (
     <ThemeProvider 
