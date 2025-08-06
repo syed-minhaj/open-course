8c65bf7e672ab7d4cbbcc51155d281c29c21375e
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-06 12:30:32 +0500

    refactor: update external image access in next config

--- next.config.ts
+++ next.config.ts
@@ -4,29 +4,68 @@ const nextConfig: NextConfig = {
   /* config options here */
   //output: 'standalone',
   images: {
-    domains: [
-      'i.pinimg.com',
-      'lh3.googleusercontent.com',
-      'itwolrshcebjdyoihbzg.supabase.co',
-    ],
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'i.pinimg.com',
-    //     pathname: '**',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'lh3.googleusercontent.com',
-    //     pathname: '**',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'itwolrshcebjdyoihbzg.supabase.co',
-    //     pathname: '**',
-    //   },
+    // domains: [
+    //   'i.pinimg.com',
+    //   'lh3.googleusercontent.com',
+    //   'itwolrshcebjdyoihbzg.supabase.co',
     // ],
+    
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'itwolrshcebjdyoihbzg.supabase.co',
+        port: '',
+        pathname: '**',
+      },
+    ],
   },
+  async headers() {
+    return [
+      {
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/supabase-images/:path*',
+  //       headers: [
+  //         {
+  //           key: 'Cache-Control',
+  //           value: 'public, max-age=31536000, immutable',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/supabase-images/:path*',
+  //       destination: 'https://itwolrshcebjdyoihbzg.supabase.co/storage/v1/object/public/:path*',
+  //     },
+  //   ];
+  // },
+
 
   // webpack: (config, { webpack }) => {
   //   config.resolve.fallback = {
