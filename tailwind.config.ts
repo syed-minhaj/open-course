bafd68b4be02b717160e87ce921c053abaa8e919
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-10-05 06:46:51 +0500

    feat: shadcn added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import  color  from "tailwindcss/colors";
 
 export default {
   content: [
@@ -17,6 +16,32 @@ export default {
         secondary: "var(--color-secondary)",
         tertiary: "var(--color-tertiary)",
         accent: "var(--color-accent)",
+        card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
       },
       height: {
         "screen": "100dvh",
@@ -35,6 +60,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
   darkMode: "class",
 } satisfies Config;
